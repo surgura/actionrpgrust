cb538d7afd714622460b6daff793ae674983779f
Aart Stuurman <aart@astuurman.com>
2021-08-29 16:32:23 +0200

    Add basic animation to player

--- src/player.rs
+++ src/player.rs
@@ -6,12 +6,18 @@ use super::render_target_3dsprites::RenderTarget3DSprites;
 
 pub struct Player {
     position: Vector3<f32>,
+    left_foot_animation_state: u32,
+    anim_timer: u32,
+    left_foot_pos: Vector3<f32>,
 }
 
 impl Player {
     pub fn new() -> Player {
         Player {
             position: Vector3::new(100.0, 0.0, 0.0),
+            left_foot_animation_state: 0,
+            anim_timer: 0,
+            left_foot_pos: Vector3::new(0.0, 0.0, 0.0),
         }
     }
 
@@ -34,9 +40,49 @@ impl Player {
         if input.is_key_down(KeyboardKey::KEY_F) {
             self.position[2] -= 1.0;
         }
+
+        if input.is_key_pressed(KeyboardKey::KEY_SPACE) {
+            self.left_foot_animation_state += 1;
+            self.left_foot_animation_state %= 3;
+        }
+
+        self.anim_timer += 1;
+        if self.anim_timer >= 8 {
+            self.anim_timer = 0;
+            self.left_foot_animation_state += 1;
+            self.left_foot_animation_state %= 3;
+        }
+
+        let anim_up = [
+            Vector3::new(-6.0, 0.0, 8.0),
+            Vector3::new(-6.0, 8.0, 0.0),
+            Vector3::new(-6.0, -8.0, 0.0),
+        ];
+
+        let anim_right = [
+            Vector3::new(0.0, -6.0, 8.0),
+            Vector3::new(8.0, -6.0, 0.0),
+            Vector3::new(-8.0, -6.0, 0.0),
+        ];
+
+        let anim = anim_right;
+
+        let target: &Vector3<f32> = &anim[self.left_foot_animation_state as usize];
+
+        self.left_foot_pos += (target - self.left_foot_pos) * 0.2;
     }
 
     pub fn draw(&self, target: &mut RenderTarget3DSprites) {
-        target.draw_circle(self.position, 64.0, Color::PURPLE);
+        target.draw_circle(
+            self.position + Vector3::new(0.0, 0.0, 12.0),
+            16.0,
+            Color::PURPLE,
+        );
+        target.draw_circle(self.position + self.left_foot_pos, 8.0, Color::GREEN);
+        /*target.draw_circle(
+            self.position + Vector3::new(8.0, 0.0, 0.0),
+            8.0,
+            Color::GREEN,
+        );*/
     }
 }
