ef055d5a1c48ffc97ad5290f42b7794f8614101e
Aart Stuurman <aart@astuurman.com>
2021-08-29 19:08:13 +0200

    Added bounding boxes to physics. Created get_collisions stub

--- src/physics/environment.rs
+++ src/physics/environment.rs
@@ -1,40 +1,53 @@
 use super::bounding_box::BoundingBox;
-use super::object::Object;
+use super::object::{Object, ObjectImpl};
 use std::cell::RefCell;
 use std::rc::Rc;
 
 pub struct Environment {
-    inner: Rc<RefCell<EnvironmentImpl>>,
+    inner: RefCell<EnvironmentImpl>,
 }
 
 impl Environment {
     pub fn new() -> Environment {
         Environment {
-            inner: Rc::new(RefCell::new(EnvironmentImpl::new())),
+            inner: RefCell::new(EnvironmentImpl::new()),
         }
     }
 
     /// Add an object to the environment.
     /// Remains in the environment until returned object ref drops.
-    pub fn new_object(&mut self, bounding_box: BoundingBox) -> Object {
-        Object::new(
-            self.inner.as_ref().borrow_mut().new_object(),
-            self.inner.clone(),
-            bounding_box,
-        )
+    pub fn new_object<'a>(&'a mut self, bounding_box: BoundingBox) -> Object<'a> {
+        let object = self.inner.borrow_mut().new_object(bounding_box);
+        Object::new(&self.inner, object)
+    }
+
+    pub fn get_collisions(&self, bounding_box: BoundingBox) -> Vec<u32> {
+        todo!();
+        Vec::new()
     }
 }
 
-pub struct EnvironmentImpl {}
+pub struct EnvironmentImpl {
+    // TODO this datastructure can be significantly improved
+    objects: Vec<Rc<RefCell<ObjectImpl>>>,
+}
 
 impl EnvironmentImpl {
     pub fn new() -> EnvironmentImpl {
-        EnvironmentImpl {}
+        EnvironmentImpl {
+            objects: Vec::new(),
+        }
     }
 
-    fn new_object(&mut self) -> u32 {
-        0
+    fn new_object(&mut self, bounding_box: BoundingBox) -> Rc<RefCell<ObjectImpl>> {
+        self.objects.push(Rc::new(RefCell::new(ObjectImpl::new(
+            self.objects.len() as u32,
+            bounding_box,
+        ))));
+        Rc::clone(self.objects.last().unwrap())
     }
 
-    pub fn remove_object(&mut self, object_id: u32) {}
+    pub fn remove_object(&mut self, object_id: u32) {
+        todo!();
+    }
 }
--- src/physics/object.rs
+++ src/physics/object.rs
@@ -4,42 +4,48 @@ use nalgebra::Vector3;
 use std::cell::RefCell;
 use std::rc::Rc;
 
-pub struct Object {
-    object_id: u32,
-    environment: Rc<RefCell<EnvironmentImpl>>,
-
-    position: Vector3<f32>,
-    pub bounding_box: BoundingBox,
+pub struct Object<'a> {
+    environment: &'a RefCell<EnvironmentImpl>,
+    inner: Rc<RefCell<ObjectImpl>>,
 }
 
-impl Object {
+impl<'a> Object<'a> {
     pub fn new(
-        object_id: u32,
-        environment: Rc<RefCell<EnvironmentImpl>>,
-        bounding_box: BoundingBox,
-    ) -> Self {
-        Self {
-            object_id,
-            environment,
-            position: Vector3::new(0.0, 0.0, 0.0),
-            bounding_box,
-        }
+        environment: &'a RefCell<EnvironmentImpl>,
+        inner: Rc<RefCell<ObjectImpl>>,
+    ) -> Object<'a> {
+        Self { environment, inner }
     }
 
     pub fn get_position(&self) -> Vector3<f32> {
-        self.position
+        self.inner.borrow().position
     }
 
     pub fn set_position(&mut self, new_position: Vector3<f32>) {
-        self.position = new_position;
+        self.inner.borrow_mut().position = new_position;
     }
 }
 
-impl Drop for Object {
+impl<'a> Drop for Object<'a> {
     fn drop(&mut self) {
         self.environment
-            .as_ref()
             .borrow_mut()
-            .remove_object(self.object_id);
+            .remove_object(self.inner.borrow_mut().object_id);
+    }
+}
+
+pub struct ObjectImpl {
+    object_id: u32,
+    position: Vector3<f32>,
+    bounding_box: BoundingBox,
+}
+
+impl ObjectImpl {
+    pub fn new(object_id: u32, bounding_box: BoundingBox) -> Self {
+        Self {
+            object_id,
+            position: Vector3::new(0.0, 0.0, 0.0),
+            bounding_box,
+        }
     }
 }
